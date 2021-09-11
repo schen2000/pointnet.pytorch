8912881681dbf27d1373e14c81ffb358feceffec
Sherman Chen <schen@simviu.com>
2021-09-11 12:16:17 +0800

    run_cls.sh

--- run_cls.sh
+++ run_cls.sh
@@ -1,5 +1,5 @@
 cd utils
-DATASET=../../visiond/modelnet40_normal_resampled/
+DATASET=../../../data/visiond/modelnet40_normal_resampled/
 
 python train_classification.py --dataset $DATASET --nepoch=200  --dataset_type modelnet40
 
