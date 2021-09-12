4f33e60e698776ee2e9cc4b29d27b500ad0e3acc
Sherman Chen <schen@simviu.com>
2021-09-12 12:36:24 +0800

    run GPU train OK

--- run_cls.sh
+++ run_cls.sh
@@ -1,6 +1,6 @@
 cd utils
 DATASET=../../../data/visiond/modelnet40_normal_resampled/
 
-python train_classification.py --dataset $DATASET --nepoch=200  --dataset_type modelnet40
+python train_classification.py --batchSize=16 --dataset $DATASET --nepoch=200  --dataset_type modelnet40
 
 
